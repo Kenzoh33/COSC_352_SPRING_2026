258138334430e861a70e9ce0b93c997658e42c71
macro611 <macro6@morgan.edu>
2026-04-24 21:56:11 -0400

    Delete marcus_crosby/project08/error.rs

--- marcus_crosby/project08/error.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-#[derive(Debug, Error)]
-pub enum CsvProfError {
-    #[error("invalid percentile `{0}`; expected a number in the inclusive range 0..=100")]
-    InvalidPercentile(String),
-    #[error("delimiter must be a single-byte character, got `{0}`")]
-    InvalidDelimiter(char),
-}
